9da1d2e554687ef9ca33023cd495aae2a81338a5
Davis Phillips <davis.phillips@gmail.com>
2022-01-28 09:58:50 -0600

    Update tcset_br-ex_rate20000kbps_delay115.0.sh

--- tcset_br-ex_rate20000kbps_delay115.0.sh
+++ tcset_br-ex_rate20000kbps_delay115.0.sh
@@ -22,6 +22,12 @@ modprobe ifb
 /usr/sbin/tc qdisc add dev ifb5268 parent 1494:42 handle 2cfb: netem delay 115.0ms
 /usr/sbin/tc filter add dev ifb5268 protocol ip parent 1494: prio 5 u32 match ip dst 0.0.0.0/0 match ip src 0.0.0.0/0 flowid 1494:42
 
+# Remove Incoming filter for either
+/usr/sbin/tc qdisc del dev br-ex ingress
+/usr/sbin/tc qdisc del dev ifb5268 root 
+/usr/sbin/ip link set dev ifb5268 down
+/usr/sbin/ip link delete ifb5268
+
 # Outgoing IPv6
 /usr/sbin/tc qdisc add dev br-ex root handle 1494: htb default 1
 /usr/sbin/tc class add dev br-ex parent 1494: classid 1494:1 htb rate 32000000.0kbit
@@ -30,9 +36,11 @@ modprobe ifb
 /usr/sbin/tc filter add dev br-ex protocol ipv6 parent 1494: prio 6 u32 match ip6 dst ::/0 match ip6 src ::/0 flowid 1494:158
 
 # Outgoing IPv4
-
 /usr/sbin/tc qdisc add dev br-ex root handle 1494: htb default 1
 /usr/sbin/tc class add dev br-ex parent 1494: classid 1494:1 htb rate 32000000.0kbit
 /usr/sbin/tc class add dev br-ex parent 1494: classid 1494:42 htb rate 20000.0Kbit ceil 20000.0Kbit burst 2500.0KB cburst 2500.0KB
 /usr/sbin/tc qdisc add dev br-ex parent 1494:42 handle 2cfb: netem delay 115.0ms
 /usr/sbin/tc filter add dev br-ex protocol ip parent 1494: prio 5 u32 match ip dst 0.0.0.0/0 match ip src 0.0.0.0/0 flowid 1494:42
+
+# Remove the filter for either
+/usr/sbin/tc qdisc del dev br-ex root
